8902879a3ab4e40cb472bc9360ae9c700c169196
RohanBhandari <rohanbhandari11@gmail.com>
2016-06-02 15:44:48 -0700

    Output fill scheme as txt file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Misc
 Miscellaneous Scripts
+
+1) findFillScheme.py
+-- Scrapes the fill scheme from a saved (offline) WBM "Bunch Fill" page
+-- Needs the following packages: BeautifulSoup4 and lxml
+
+
--- findFillScheme.py
+++ findFillScheme.py
@@ -1,10 +1,30 @@
+# Scrapes the fill scheme from a saved (offline) WBM "Bunch Fill" page
+# Needs the following packages: BeautifulSoup4 and lxml
+
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(open("/Users/rohan/Desktop/Bunch4961Fill.html"),"lxml")
+def hasCollision(bunch):
+    idx_true=bunch.find("true")
+    idx_false=bunch.find("false")
+
+    #Only collision if all are true (i.e. no false)
+    if idx_false==-1:
+        return "True"
+    else:
+        return "False"
+
+dir = "/Users/rohan/Desktop/"
+fill = "BunchFill4961"
+
+soup = BeautifulSoup(open(dir+fill+".html"),"lxml")
 
 bunchMap = soup.map
-
 bunches = [area for area in bunchMap.findAll("area")]
 
+f = open(fill+".txt","w")
+
 for bunch in bunches:
-    print(bunch)
+    bunch_clean = hasCollision(str(bunch))
+    
+    f.write(bunch_clean+"\n")
+
