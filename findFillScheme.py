3a6736ecd59b2e6e29a56cb7dc5a2d90084839ee
RohanBhandari <rohanbhandari11@gmail.com>
2016-06-03 13:47:14 -0700

    Added check to make sure bunch file exists before scraping

--- findFillScheme.py
+++ findFillScheme.py
@@ -2,6 +2,8 @@
 # Needs the following packages: BeautifulSoup4 and lxml
 
 from bs4 import BeautifulSoup
+import os.path
+import sys
 
 def hasCollision(bunch):
     idx_true=bunch.find("true")
@@ -14,23 +16,26 @@ def hasCollision(bunch):
         return "0"
 
 def findOneFillScheme(path,filename,outdir):
+    
+    if not os.path.isfile(path+filename+".html"):
+        sys.exit("ERROR: WBM file for "+filename+" does not exist!")
 
     soup = BeautifulSoup(open(path+filename+".html"),"lxml")
     
     bunchMap = soup.map
     bunches = [area for area in bunchMap.findAll("area")]
     
-    f = open(outdir+fill+".txt","w")
+    f = open(outdir+filename+".txt","w")    
     
     for bunch in bunches:
         bunch_clean = hasCollision(str(bunch))
         f.write(bunch_clean+"\n")
     f.close()
-    print("Saved "+outdir+fill+".txt")
+    print("Saved "+outdir+filename+".txt")
 
 #Main Loop
 dir = "/Users/rohan/Desktop/"
-fills = ["BunchFill4919","BunchFill4924","BunchFill4961","BunchFill4964"]
+fills = ["BunchFill4919","BunchFill4924","BunchFill4925","BunchFill4926","BunchFill4930","BunchFill4961","BunchFill4964"]
 outdir = "fillschemes/"
 
 for fill in fills:
