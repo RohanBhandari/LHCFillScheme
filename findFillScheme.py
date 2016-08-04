fa21aacb7846388ba1d691c7ccedaba689c8b15f
root <root@SERF.localdomain>
2016-08-04 14:44:54 -0400

    Updated to Fill 5154 (Run2016F)

--- findFillScheme.py
+++ findFillScheme.py
@@ -33,33 +33,39 @@ def findOneFillScheme(path,filename,outdir):
     print("Saved "+outdir+filename+".txt")
 
 #Main Loop
-dir = "/Users/rohan/Desktop/"
+#dir = "/Users/rohan/Desktop/"
+dir = "/root/Desktop/bunch_temp/"
 outdir = "fillschemes/"
 
 #2016
 fills = [
     #Run2016B
-#    "BunchFill4888","BunchFill4889","BunchFill4890","BunchFill4892","BunchFill4895",
-#    "BunchFill4896","BunchFill4905","BunchFill4906","BunchFill4910","BunchFill4915",
-#    "BunchFill4919","BunchFill4924","BunchFill4925","BunchFill4926","BunchFill4930",
-#    "BunchFill4935","BunchFill4937","BunchFill4942","BunchFill4945","BunchFill4947",
-#    "BunchFill4953","BunchFill4954","BunchFill4956","BunchFill4958","BunchFill4960",
-#    "BunchFill4961","BunchFill4964","BunchFill4965","BunchFill4976","BunchFill4979",
-#    "BunchFill4980","BunchFill4984","BunchFill4985","BunchFill4988","BunchFill4990",
-#    "BunchFill5005","BunchFill5013","BunchFill5017","BunchFill5020","BunchFill5021",
-#    "BunchFill5024","BunchFill5026","BunchFill5027","BunchFill5028","BunchFill5029",
-#    "BunchFill5030",
+    "BunchFill4888","BunchFill4889","BunchFill4890","BunchFill4892","BunchFill4895",
+    "BunchFill4896","BunchFill4905","BunchFill4906","BunchFill4910","BunchFill4915",
+    "BunchFill4919","BunchFill4924","BunchFill4925","BunchFill4926","BunchFill4930",
+    "BunchFill4935","BunchFill4937","BunchFill4942","BunchFill4945","BunchFill4947",
+    "BunchFill4953","BunchFill4954","BunchFill4956","BunchFill4958","BunchFill4960",
+    "BunchFill4961","BunchFill4964","BunchFill4965","BunchFill4976","BunchFill4979",
+    "BunchFill4980","BunchFill4984","BunchFill4985","BunchFill4988","BunchFill4990",
+    "BunchFill5005","BunchFill5013","BunchFill5017","BunchFill5020","BunchFill5021",
+    "BunchFill5024","BunchFill5026","BunchFill5027","BunchFill5028","BunchFill5029",
+    "BunchFill5030",
 
     #Run2016C
-#    "BunchFill5038","BunchFill5043","BunchFill5045","BunchFill5048","BunchFill5052",
-#    "BunchFill5056","BunchFill5059","BunchFill5060","BunchFill5069","BunchFill5071",
+    "BunchFill5038","BunchFill5043","BunchFill5045","BunchFill5048","BunchFill5052",
+    "BunchFill5056","BunchFill5059","BunchFill5060","BunchFill5069","BunchFill5071",
 
     #Run2016D
     "BunchFill5072","BunchFill5073","BunchFill5076","BunchFill5078","BunchFill5080",
     "BunchFill5083","BunchFill5085","BunchFill5091","BunchFill5093","BunchFill5095",
 
     #Run2016E
-    "BunchFill5096","BunchFill5097"
+    "BunchFill5096","BunchFill5097","BunchFill5101","BunchFill5102","BunchFill5105",
+    "BunchFill5106","BunchFill5107","BunchFill5108","BunchFill5109","BunchFill5110",
+    "BunchFill5111","BunchFill5112","BunchFill5116","BunchFill5117",
+
+    #Run2016F
+    "BunchFill5149","BunchFill5151","BunchFill5154"
     ]
 
 for fill in fills:
