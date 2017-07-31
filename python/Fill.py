7eeac371764877bb6db9aa13940a21017c525a55
Rohan Bhandari <rohan.bhandari@cern.ch>
2017-07-31 06:01:48 -0700

    Added fill number field to class (python only atm)

--- python/Fill.py
+++ python/Fill.py
@@ -1,6 +1,8 @@
 class Fill:
     def __init__(self, file, runlist=[]):
 
+        self.fillN = self.getFillNumber(file)
+        self.runs = runlist
         self.isoBXs    = []
         self.firstBXs  = []
         self.lastBXs   = []
@@ -11,8 +13,6 @@ class Fill:
 
         scheme = self.readFile(file)
         self.getBXs(scheme)
-
-        self.runs = runlist
 
     def readFile(self, filepath):    
         scheme = []
@@ -46,3 +46,6 @@ class Fill:
         #Add last BX, which is the last bunch of the abort gap and is always empty
         #In the CMS numbering scheme this is BX=0, but HCAL PFG tuples follows the LHC numbering scheme in which this is BX=3564
         self.emptyBXs.append(3564)
+
+    def getFillNumber(self, file):
+        return int(file.split("/")[-1].split("bunchpattern")[-1].split(".txt")[0])
